68c65cf60f7a728009ce90ffb425ce02ba23f991
adityaranote <rajputaditya88@gmail.com>
2022-10-05 18:09:44 +0530

    day 3 completed routes created and added some thing to text

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'package:my_app/pages/login_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      // home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      darkTheme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home": (context) => HomePage(),
+        "/login": (context) => LoginPage(),
+      },
     );
   }
 }
