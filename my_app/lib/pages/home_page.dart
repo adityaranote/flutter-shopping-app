043782caa21901bfcbdaf35ad5fbabcaf79d70a5
adityaranote <rajputaditya88@gmail.com>
2022-10-11 09:46:55 +0530

    conetxt is the loaction of element in widget tree

--- my_app/lib/pages/home_page.dart
+++ my_app/lib/pages/home_page.dart
@@ -14,10 +14,14 @@ class HomePage extends StatelessWidget {
           "Catalog App",
         ),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome to ${days} days of flutter by ${name}"),
-        ),
+      body: Column(
+        children: [
+          Container(
+            child: Text(
+              "welcome to flutter practice app",
+            ),
+          ),
+        ],
       ),
       drawer: MyDrawer(),
     );
